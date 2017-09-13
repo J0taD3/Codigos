91c7af91eb7a15172c8ef122a140f914cc6e9d64
J.D <30913200+J0taD3@users.noreply.github.com>
2017-09-13 10:31:23 -0300

    Trabalho
    Trabalho

---
+++ Matriz secundária.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <locale.h>
+#define m 5
+#define n 5
+
+int main () {
+	setlocale(LC_ALL, "portuguese_Brazil");
+	int a, b, c[m][n];
+	for(a=0;a<m;a++){
+		printf("\n\t\t Preencha a linha %i: \n",a+1);
+		for(b=0;b<n;b++){
+			printf("Dê um valor pra coluna %i: ",b+1);
+			scanf("%i",&c[a][b]);
+		}
+	}
+	for(a=0;a<m;a++){
+		for(b=0;b<n;b++){
+			printf("\n Para [%i] [%i]: %i.",a+1, b+1,c[a][b]);
+		}
+	}	
+	
+	
+    	for(a=0;a<m;a++){
+	      for(b=0;b<n;b++){
+	          if (b==(n-1-a)){
+	         printf("\n[%i] [%i]: %i.", a+1, b+1, c[a][b]);
+	         }
+	     } 
+    }
+return 0;
+} 
+
