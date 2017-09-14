d68a6fa3f4e9151e76f04a261b61cae00f8ae98c
J.D <30913200+J0taD3@users.noreply.github.com>
2017-09-14 11:47:51 -0300

    Trabalho de matrizes

--- Diagonal secundária.cpp
+++ Diagonal secundária.cpp
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <locale.h>
-#define m 3
+#define m 3 //tamanho da matriz (linha X coluna)
 
-int matriz ( int c[m][m]){  
+int matriz ( int c[m][m]){  //mostra a matriz completa
     int a, b;
     printf("\t\tConfira sua matriz\n");
 	for(a=0;a<m;a++){
 		for(b=0;b<m;b++){
 			printf("\t |%i|",c[a][b]);
 		}
-	  printf("\n");
+	  printf("\n");//apenas para organizar a matriz em tabela
 	}	
 }
 
-int digsec ( int c[m][m]){
+int digsec ( int c[m][m]){ //mostra apenas a diagonal secundária
     int a, b;
     printf("\t\tDiagonal Secundária\n");
     	for(a=0;a<m;a++){
 	      for(b=0;b<m;b++){
-	          if (b==(m-1-a)){
+	          if (b==(m-1-a)){ //faz a leitura da matriz e indentifica as celulas da diagonal secundária
 	          printf(" |%i|", c[a][b]);
 	         }
 	     } 
@@ -28,7 +28,7 @@ int digsec ( int c[m][m]){
 main () {
 	setlocale(LC_ALL, "portuguese_Brazil");
 	int l, p, c[m][m];
-	for(l=0;l<m;l++){
+	for(l=0;l<m;l++){ //é feita a tribuição de valores 
 		printf("\n\t\t Preencha a linha %i: \n",l+1);
 		for(p=0;p<m;p++){
 			printf("Dê um valor pra coluna %i: ",p+1);
@@ -36,8 +36,8 @@ main () {
 		}
 	}
 printf("\n");
-    matriz (c);
+    matriz (c); //chama a função para exibir a matriz completa
 printf("\n");
-    digsec (c);
+    digsec (c); //chama a função para exibir a diagonal secundária
 return 0;
-} 
+}
